feccc11c36bf626d491853d3327aaa385e7091d7
William Cotton <williamcotton@gmail.com>
2012-06-18 16:50:50 -0700

    fixed disconnect mechanism

--- cs_channel_history.c
+++ cs_channel_history.c
@@ -56,7 +56,7 @@ on_channel_message(hook_cmessage_data_t *data)
         }
         freeReplyObject(reply);
 
-        freeRedis(redis);
+        redisFree(redis);
 	}
 }
 
@@ -91,7 +91,7 @@ on_channel_join(hook_channel_joinpart_t *hdata)
     }
     freeReplyObject(reply);
     
-    freeRedis(redis);
+    redisFree(redis);
 }
 
 void _modinit(module_t *m)
