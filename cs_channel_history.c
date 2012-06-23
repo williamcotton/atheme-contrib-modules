d897b2a6e4d32eb0fb8420b6ef7f21fbedac2407
William Cotton <williamcotton@gmail.com>
2012-06-22 18:37:28 -0700

    debug

--- cs_channel_history.c
+++ cs_channel_history.c
@@ -160,7 +160,7 @@ on_channel_join(hook_channel_joinpart_t *hdata)
 		epoch_time_obj = json_object_object_get(new_obj, "epoch_time");
 		
 		if ((long)epoch_time_obj == 1) {
-			return;
+			break;
 		}
 		
 		printf("(int)epoch_time_obj: %li\n",(long)epoch_time_obj);
